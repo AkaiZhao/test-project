b63d53e8312a2e480994dcc8420326d97bcbde7d
akai <akai-rnd@skvip.org>
2024-07-01 18:15:56 +0800

    fix

--- src/components/counter/index.tsx
+++ src/components/counter/index.tsx
@@ -2,11 +2,13 @@
 
 import { create } from 'zustand'
 
-const useStore = create<{ count: number; inc: () => void }>((set) => ({
-  count: 1,
-  inc: () => set((state) => ({ count: state.count + 1 })),
-  dec: () => set((state) => ({ count: state.count - 1 })),
-}))
+const useStore = create<{ count: number; inc: () => void; dec: () => void }>(
+  (set) => ({
+    count: 1,
+    inc: () => set((state) => ({ count: state.count + 1 })),
+    dec: () => set((state) => ({ count: state.count - 1 })),
+  }),
+)
 
 export default function Counter() {
   const { count, inc, dec } = useStore()
